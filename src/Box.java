ff58067782c301a15dd151334238aa5e6cbca70d
Батаева Елена Валентиновна <EBataeva@stream.ru>
2021-02-19 19:38:53 +0300

    опять задание 11

---
+++ src/Box.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Box {
+    private ArrayList<Fruit> list = new ArrayList<>();
+
+
+    public void putFruitToBox(Fruit fruit, int count) {
+
+        if ((this.getList().size() !=0 && this.getList().get(0) == fruit) || this.getList().size() == 0) { //проверяем, что либо коробка пустая, либо в ней такие же фрукты
+            for (int i = 0; i < count; i++) {
+                list.add(fruit);
+            }
+        }
+    }
+
+
+
+    public Float getWeight() {
+        Float sum = 0.0f;
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i).getWeight();
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    public ArrayList<Fruit> getList() {
+        return list;
+    }
+
+    /*
+    public static Boolean compare(Box box1, Box box2) {
+        if (box1.getList().get(0) == (box2.getList().get(0)) && box1.getList().size() == box2.getList().size()) {
+            return true;
+        }
+        return false;
+        }
+    */
+    public Boolean compare(Box box) {
+        if (this.getList().get(0) == (box.getList().get(0)) && this.getList().size() == box.getList().size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void sendToCard(Box box) {
+        if (this.getList().get(0) == (box.getList().get(0))) {
+            for (Fruit f : this.getList()) {
+                box.getList().add(f);
+            }
+            for (int i = 0; i < this.getList().size() ; i++) {
+                this.getList().clear();
+            }
+
+
+        }else{
+            System.out.println("нельзя так делать. типы не соответствуют");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+  static  String[] numbers = {"раз", "два", "три", "четыре"};
+    static Integer[] nums = {1,2,3,4,5};
+
+    public static void main(String[] args) {
+        testFirstPartOfHomework();
+        Fruit apple = new Apple();
+        Fruit orange = new Orange();
+        Box box1 = new Box();
+        Box box2 = new Box();
+        Box box3 = new Box();
+        box1.putFruitToBox(apple, 10);
+        box2.putFruitToBox(orange, 15);
+        box3.putFruitToBox(apple, 2);
+        box3.sendToCard(box1);
+        box1.putFruitToBox(apple, 1);
+        System.out.println("вес 3 = "+box3.getWeight());
+        System.out.println("вес 1 = "+box1.getWeight());
+        System.out.println(box1.compare(box2));
+
+    }
+
+    private static void testFirstPartOfHomework() {
+        swap(numbers, 1, 2);
+        System.out.println(Arrays.toString(numbers));
+        swap(nums, 1, 2);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(changeArray(numbers));
+    }
+
+    public static <T> void swap(T[]arr, int index, int index2){ //меняет элементы массива местами
+        if(index<arr.length && index2 < arr.length){
+        T temp = arr[index];
+        arr[index]= arr[index2];
+        arr[index2] = temp;}
+        else{
+            System.out.println("нет элементов с таким индексом");
+        }
+    }
+
+
+    public static <T> ArrayList changeArray(T[] arr){
+        ArrayList <T> list =  new ArrayList<>();
+        for(T a : arr){
+            list.add(a);
+        }
+        return list;
+    }
+}
